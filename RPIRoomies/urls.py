486b654319cb82b0b12b36c5b0f6592c14b515c5
Joe Pringle <explodingp@gmail.com>
2014-11-21 01:43:03 -0500

    use python social auth to validate users

--- RPIRoomies/urls.py
+++ RPIRoomies/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'RPIRoomies.views.index'),
+    url('', include('social.apps.django_app.urls', namespace='social')),
 )
