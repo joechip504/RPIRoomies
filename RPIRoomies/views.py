9845a249b6d1b19e76851ab4852d226b5e1dcb9b
Joe Pringle <explodingp@gmail.com>
2014-11-23 12:35:05 -0500

    add 'find' page and fix navbar links

--- RPIRoomies/views.py
+++ RPIRoomies/views.py
@@ -8,3 +8,10 @@ def index(request):
     'request': request
      }
     return render(request, 'index.html', context)
+
+def find(request):
+    context = { 
+    'user': request.user,
+    'request': request
+     }
+    return render(request, 'find.html', context)
